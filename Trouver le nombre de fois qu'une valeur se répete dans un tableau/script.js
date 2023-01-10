92605b25b6adbe7e1fcce62df035f9dd17eaabbf
Fidgino <Fidgino@gmail.com>
2023-01-10 16:00:17 +0200

    Uptade

--- Trouver le nombre de fois qu'une valeur se répete dans un tableau/script.js
+++ Trouver le nombre de fois qu'une valeur se répete dans un tableau/script.js
@@ -1,8 +1,12 @@
 let arr = [1, 2, 1, 1, 2, 5, 4, 9, 7, 8, 7, 9, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,];
+
+function TrouveChien(word) {
 let res = [];
-let val = arr.indexOf(5);
+let val = arr.indexOf(word);
 while(val != -1){
     res.push(val);
-    val = arr.indexOf(5, val + 1);
+    val = arr.indexOf(word, val + 1);
 }
 console.log(`l'élément est présent ${res.length} fois`);
+}
+TrouveChien(9);
